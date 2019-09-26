d0671e66683decb4be08f00cb5411d55e112408a
MrBradabie <55830472+MrBradabie@users.noreply.github.com>
2019-09-26 16:53:54 +0700

    Add files via upload

---
+++ PBO1-10118006-Latihan13-Relational/src/pbo1/pkg10118006/latihan13/relational/PBO110118006Latihan13Relational.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo1.pkg10118006.latihan13.relational;
+
+/**
+ *
+ * @author 
+ * NAMA  : Alfarizi Rizki Pane
+ * KELAS : PBO1
+ * NIM   : 10118006
+ * Deskripsi Program : Program ini berisi  program untuk relational
+ */
+public class PBO110118006Latihan13Relational {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int bil1 = 10;
+        int bil2 = 20;
+        System.out.println("a == b = " + (bil1 == bil2));
+        System.out.println("a != b = " + (bil1 != bil2));
+        System.out.println("a > b = " + (bil1 > bil2));
+        System.out.println("a < b = " + (bil1 < bil2));
+        System.out.println("b >= a = " + (bil2 >= bil1));
+        System.out.println("b <= a = " + (bil2 <= bil1));
+    }
+    
+}
